334a15afe3cca6f4da94ff54b061981f646bee55
JOSE TUÑON <ut_josetv@hotmail.com>
2025-09-02 08:32:24 -0500

    Se realiza ajuste en controlador de drivers

--- src/drivers/drivers.controller.ts
+++ src/drivers/drivers.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, ParseArrayPipe } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { DriversService } from './drivers.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
@@ -36,10 +36,18 @@ export class DriversController {
   }
   @Post() create(@Body() data: CreateDriverDto) { return this.service.create(data); }
   @Post('bulk')
-  createMany(
-    @Body(new ParseArrayPipe({ items: CreateDriverDto })) data: CreateDriverDto[],
-  ) {
-    return this.service.createMany(data);
+  createMany(@Body() body: any) {
+    let data: any = body;
+    if (typeof data === 'string') {
+      try { data = JSON.parse(data); } catch { /* ignore */ }
+    }
+    if (data && !Array.isArray(data) && Array.isArray((data as any).items)) {
+      data = (data as any).items;
+    }
+    if (!Array.isArray(data)) {
+      throw new BadRequestException('Body must be a JSON array of CreateDriverDto');
+    }
+    return this.service.createMany(data as CreateDriverDto[]);
   }
   @Put(':id') update(@Param('id') id: string, @Body() data: UpdateDriverDto) { return this.service.update(Number(id), data); }
   @Delete(':id') remove(@Param('id') id: string) { return this.service.remove(Number(id)); }
