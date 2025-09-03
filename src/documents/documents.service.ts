c6e75d5d86b46432c980e607afbdfcf12b0fd3e7
JOSE TUÑON <ut_josetv@hotmail.com>
2025-09-03 12:33:51 -0500

    Se realizan ajustes en certificado ingresos

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -114,7 +114,7 @@ export class DocumentsService {
     if (existsSync(unicodeFont)) { doc.font(unicodeFont); } else { doc.font('Helvetica'); }
     doc.text(
       sanitize(`, identificado con la cédula de ciudadanía No. ${ownerIdentification}, tiene afiliado a esta empresa un vehículo de su propiedad desde el ${entryDate}, cuyas características se detallan más adelante,`),
-      { align: 'justify', lineBreak: false },
+      { align: 'justify', continued: true },
     );
 
     // Immediately continue with income sentence after the previous phrase
